88b547b2f975faa9577c9a766b8ebc888fd25077
Blu <plush.gamer.456@gmail.com>
2024-03-01 14:26:26 -0500

    Revert "ugh"
    This reverts commit ae9e4d03a51de03c13368c69389dee86dc2ba1c1.

--- src/index.tsx
+++ src/index.tsx
@@ -13,11 +13,7 @@ app.get('/', (c) => {
 const port = 2258
 console.log(`Server is running on port ${port}`)
 
-<<<<<<< HEAD
 // push
-=======
-// push2
->>>>>>> parent of c430967 (Last Leaderboard entry styling)
 
 serve({
   fetch: app.fetch,
